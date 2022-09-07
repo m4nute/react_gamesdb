1363479a847cd07b197490ebf5d0d0fa0f97bc4c
m4nutee <mgudinocontacto@gmail.com>
2022-09-06 22:39:20 -0300

    minified removed

--- src/App.js
+++ src/App.js
@@ -1,4 +1,22 @@
 import React from 'react'
-import 'bootswatch/dist/lux/bootstrap.min.css';import '../src/App.css'
-import Navbar from './Navbar';import{Route,Routes}from 'react-router-dom';import Home from './Home';import Database from './Database';function App(){return(<div><div className="nav"><Navbar></Navbar></div><div className="columns"><Routes><Route path='/' element={<Home/>}></Route><Route path='/database' element={<Database/>}></Route></Routes></div></div>)}
+import 'bootswatch/dist/lux/bootstrap.min.css';
+import '../src/App.css'
+import Navbar from './Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './Home';
+import Database from './Database';
+
+function App() {
+    return (
+        <div>
+            <div className="nav" >
+                <Navbar />
+            </div>
+            <div className="columns" ><Routes><Route path='/' element={<Home />} />
+                <Route path='/database' element={<Database />} />
+            </Routes>
+            </div>
+        </div>)
+}
+
 export default App
